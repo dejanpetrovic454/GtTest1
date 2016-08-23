72cd3b76fc986a881072e2aff3b99ddd531bf5c6
dejanp <dejanp@N-DPETROVIC>
2016-08-24 08:46:19 +1000

    Added 'a'

--- src/GitTest1/Controllers/HomeController.cs
+++ src/GitTest1/Controllers/HomeController.cs
@@ -15,6 +15,8 @@ namespace GitTest1.Controllers
 
         public IActionResult About()
         {
+            int a = 1;
+
             ViewData["Message"] = "Your application description page. ";
 
             return View();
